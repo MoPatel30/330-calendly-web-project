c6444f834af8943e0cc35621a7633e74ffd78720
Mohammed Patel <mopatel1214@gmail.com>
2021-02-09 17:57:32 -0600

    very basic profile component going

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Profile from "./Profile/Profile.jsx"
 
 
 function App() {
   return (
     <div className="App">
       <h1>Calendar Project</h1>
+
+      <Profile />
     </div>
   );
 }
