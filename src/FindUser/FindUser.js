838528d621303de26bb97af76a2ec5c0542983bc
Kevin Guillaume <keving3492@gmail.com>
2021-04-27 19:14:28 -0500

    Meeting Display in Modal

--- src/FindUser/FindUser.js
+++ src/FindUser/FindUser.js
@@ -3,6 +3,8 @@ import { db } from "../firebase"
 import Dialog from '@material-ui/core/Dialog';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
+import Calendar from 'react-calendar'
+
 
 import './FindUser.css'
 function FindUser() {
@@ -11,6 +13,14 @@ function FindUser() {
     //Variable for true/false if open + The clicked user info
     const [open, setOpen] = useState(false);
     const [clickUser,setClickUser] = useState([])
+
+    //start time val for DB
+    var  startTime = [];
+
+    //Date variables for modal calendar
+    const[calDate,setCalDate] = useState(new Date());
+
+    var dayName = calDate.toUTCString().substring(0,16)
 
     //Array for search
     const [filteredUsers, setFilteredUsers] = useState([])
@@ -28,7 +38,18 @@ function FindUser() {
       const handleClose = () => {
         setOpen(false)
       }
-
+      function tConvert (time) {
+        // Check correct time format and split into components
+        time = time.toString ().match (/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [time];
+      
+        if (time.length > 1) { // If time format correct
+          time = time.slice (1);  // Remove full string match value
+          time[5] = +time[0] < 12 ? 'AM' : 'PM'; // Set AM/PM
+          time[0] = +time[0] % 12 || 12; // Adjust hours
+        }
+        return time.join (''); // return adjusted time or original string
+      }
+      
     //Gets Users from DB
     useEffect(() => { 
         db.collection('users').onSnapshot(snapshot => {          
@@ -80,6 +101,53 @@ function FindUser() {
                 </Toolbar>
                 <div className="title-open">
                     Check Out {clickUser.name}'s Schedule
+                    <Calendar 
+                        value = {calDate}
+                        onChange={setCalDate}
+                    /> 
+                    {clickUser[dayName] ? (
+                    //sets the second param for db access
+                    Object.keys(clickUser[dayName]).map( (key) => (
+                        startTime.push(key)
+                    )),
+                    //display contents of second param usgin clickUser[dayName][startTime].name,.X.Y.Z....
+                    startTime.map((time) =>(
+                        <div style={{color: "blue", borderBottom: "1px solid black"}}>
+                            <div>
+                            Duration: {tConvert(time)} - {tConvert(clickUser[dayName][time].endTime)}
+                            </div>
+                            <div>
+                            Number of People allowed: {clickUser[dayName][time].maxNumOfPeople}
+                            </div>
+                            <div>
+                            People Joined: {clickUser[dayName][time].people.map((person)=>(
+                                <span>{person}</span>
+                            ))}
+                            </div>
+                            <div>
+                            Description: {clickUser[dayName][time].meetingDescription}
+                            </div>
+                            <div>
+                            Zoom Link: {clickUser[dayName][time].zoomLink}
+                            </div>
+                            
+                        </div>
+
+                    ))
+                    
+        
+
+
+                    )
+                    : 
+                    (
+                        <div>No Opennings Here!</div>
+
+
+                    )}
+                    
+                    
+                    
                 </div>
             </Dialog> 
         </div>
